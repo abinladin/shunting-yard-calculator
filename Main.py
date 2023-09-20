e992f1b08da1988159e413bac0b82731adeb4e94
Abdullah Binladin <abdullahbinladin98@gmail.com>
2023-09-21 00:57:59 +0300

    implemented shunting yard function

--- Main.py
+++ Main.py
@@ -14,8 +14,41 @@ def tokenizer(user_input: str, operator_list: list) -> list:
     return tokenized_list
 
 
-if __name__ == "__main__":
+def shunting_yard(tokenized_expression: list, operator_precedence: dict):
+    output = []
+    operater_stack = []
 
+    opening_parenthesis_counter = 0
+    closing_parenthesis_counter = 0
+
+    for token in tokenized_expression:
+        if token.isdigit():
+            output.append(token)
+        elif token in operator_precedence.keys():
+            while operater_stack and operator_precedence[token] <= operator_precedence[operater_stack[-1]] and operater_stack[-1] != '(':
+                output.append(operater_stack.pop())
+            operater_stack.append(token)
+        elif token == '(':
+            opening_parenthesis_counter += 1
+            operater_stack.append(token)
+        elif token == ')':
+            closing_parenthesis_counter += 1
+            while operater_stack and operater_stack[-1] != "(":
+                output.append(operater_stack.pop())
+            operater_stack.pop()
+        else:
+            continue
+
+    while operater_stack:
+        output.append(operater_stack.pop())
+
+    if opening_parenthesis_counter != closing_parenthesis_counter:
+        print("warning: parenthesis mismatch detected")
+
+    return output
+
+
+if __name__ == "__main__":
     operator_to_precedence = {
         '+': 0,
         '-': 0,
@@ -26,5 +59,10 @@ if __name__ == "__main__":
         ')': 3
     }
 
-    #tests
-    print(tokenizer("11  + 2 -   3", list(operator_to_precedence.keys())))
+    # Tests
+    expression = "1 + 1 - 3 * 12"
+    tokenized_expression = tokenizer(expression, list(operator_to_precedence.keys()))
+    rpn = shunting_yard(tokenized_expression, operator_to_precedence)
+    print("Expression: " + expression)
+    print("Tokenized Expression: " + str(tokenized_expression))
+    print("RPN: " + str(rpn))
