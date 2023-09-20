b0e8ed96c6a56d0adf0e98983793fe108d319acd
Abdullah Binladin <abdullahbinladin98@gmail.com>
2023-09-20 23:55:09 +0300

    implemented tokenizer

--- Main.py
+++ Main.py
@@ -1,2 +1,30 @@
+def tokenizer(user_input: str, operator_list: list) -> list:
+    token_builder = ""
+    tokenized_list = []
+    for char in user_input:
+        if char.isdigit():
+            token_builder += char
+        elif char in operator_list:
+            tokenized_list.append(token_builder)
+            tokenized_list.append(char)
+            token_builder = ""
+        else:
+            continue
+    tokenized_list.append(token_builder)
+    return tokenized_list
+
+
 if __name__ == "__main__":
-    print("Hello, world!")
+
+    operator_to_precedence = {
+        '+': 0,
+        '-': 0,
+        '*': 1,
+        '/': 1,
+        '^': 2,
+        '(': 3,
+        ')': 3
+    }
+
+    #tests
+    print(tokenizer("11  + 2 -   3", list(operator_to_precedence.keys())))
